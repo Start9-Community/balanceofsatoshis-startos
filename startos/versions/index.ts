4efb052d3e3ff53164e846a03a2dde13a5d36b33
Alexander Klemmstein <hardcorebtc@tutanota.com>
2026-06-01 22:26:48 +0200

    Bump package version to 22.0.1:2

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,8 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_22_0_1_1 } from './v22.0.1.1'
+import { v_22_0_1_2 } from './v22.0.1.2'
 
 export const versionGraph = VersionGraph.of({
-  current: v_22_0_1_1,
-  other: [],
+  current: v_22_0_1_2,
+  other: [v_22_0_1_1],
 })
